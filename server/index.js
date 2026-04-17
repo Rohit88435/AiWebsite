e5734e671d0a15aa4fe7482f19df776dc6c2729a
Rohit88435 <kuprajapatir@gmail.com>
2026-04-18 00:15:24 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://aiwebsite-1-ddaf.onrender.com/",
+    origin: "https://aiwebsite-1-ddaf.onrender.com",
     credentials: true,
   }),
 );
