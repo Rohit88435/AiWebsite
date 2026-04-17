7ae098dead885f89e9c60d6280dc5aa16052e799
Rohit88435 <kuprajapatir@gmail.com>
2026-04-17 23:34:25 +0530

    Update cookie settings for security enhancements

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -24,8 +24,8 @@ export const googleAuth = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -40,8 +40,8 @@ export const logOut = async (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
     });
     return res.status(200).json({ message: "logout successfully" });
   } catch (error) {
